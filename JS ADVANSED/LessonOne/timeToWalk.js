916db8ff1d1fa18b8947f62f68dbaebcba6c9145
twinsdz <twinsdz@yahoo.com>
2021-09-13 20:01:20 +0200

    solve

--- JS ADVANSED/LessonOne/timeToWalk.js
+++ JS ADVANSED/LessonOne/timeToWalk.js
@@ -1,3 +1,21 @@
-function solve(steps, meters, kmPerHour){
-    
+function solve(steps, meters, kmPerHour) {
+    steps = Number(steps);
+    meters = Number(meters);
+    kmPerHour = Number(kmPerHour);
+
+    let distanceImMeters = steps * meters;
+    let spedMeterSec = kmPerHour / 3.6;
+    let time = distanceImMeters / spedMeterSec;
+    let rest = Math.floor(distanceImMeters / 500);
+
+    let timeMin = Math.floor(time / 60);
+    let timeSec = Math.round(time - (timeMin * 60));
+    let timeHr = Math.floor(time / 3600);
+
+
+    console.log((timeHr < 10 ? "0" : "") + timeHr + ":" + (timeMin + rest < 10 ? "0" : "") + (timeMin + rest) + ":" + (timeSec < 10 ? "0" : "") + timeSec);
+
 }
+
+solve(4000, 0.60, 5);
+solve(2564, 0.70, 5.5);
