8ab7a906ce2074b0d34f6d43f9454ebcb496f84c
twinsdz <twinsdz@yahoo.com>
2021-09-19 11:59:49 +0200

    first upload

--- JS ADVANSED/Objects and Composition/calorieObject.js
+++ JS ADVANSED/Objects and Composition/calorieObject.js
@@ -1,5 +1,11 @@
-function solve(input){
-    
+function solve(input) {
+    let obj = {};
+
+    for (let i = 0; i < input.length; i += 2) {
+        obj[input[i]] = Number(input[i + 1]);
+
+    }
+    console.log(obj);
 }
 
 solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
---
+++ JS ADVANSED/Objects and Composition/constructionCrew.js
@@ -0,0 +1,16 @@
+function solve(obj) {
+    let requiredAmount = 0.1 * obj.weight * obj.experience;
+
+    if (obj.dizziness) {
+        obj.levelOfHydrated += requiredAmount;
+        obj.dizziness = false;
+    }
+    return obj;
+}
+
+console.log(solve({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}));
