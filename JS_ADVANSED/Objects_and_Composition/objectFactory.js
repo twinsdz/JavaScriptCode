47ad577882a0cb85531eaa8743f9eebd02c1d58b
twinsdz <twinsdz@yahoo.com>
2021-09-23 17:02:39 +0200

    update

--- JS_ADVANSED/Objects_and_Composition/objectFactory.js
+++ JS_ADVANSED/Objects_and_Composition/objectFactory.js
@@ -60,3 +60,55 @@ console.log(products);
 const player = products[3];
 
 player.play('Desi', 'Morning breeze bossa Nova');
+
+
+/* function factory(library, orders) {
+    const result = [];
+
+    for (let order of orders) {
+        // Create empty object and copy properties from template
+        const device = Object.assign({}, order.template);
+
+        // Compose methods from library for every item in parts
+        for (let part of order.parts) {
+            device[part] = library[part];
+        }
+
+        // Save result
+        result.push(device);
+    }
+
+    return result;
+}
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+const products = factory(library, orders);
+console.log(products); */
