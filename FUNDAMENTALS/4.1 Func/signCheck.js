009e912df9e47d53a1eb19745bd3f134e3f1b590
twinsdz <twinsdz@yahoo.com>
2021-09-24 18:01:51 +0200

    update

--- FUNDAMENTALS/4.1 Func/signCheck.js
+++ FUNDAMENTALS/4.1 Func/signCheck.js
@@ -1,3 +1,54 @@
+function signCheck(numOne, numTwo, numThree) {
+    if (numOne > 0 && numTwo > 0 || numOne < 0 && numTwo < 0) {
+        if (numThree > 0) {
+            console.log('Positive');
+        } else {
+            console.log('Negative');
+        }
+    } else {
+        if (numThree > 0) {
+            console.log('Negative');
+        } else {
+            console.log('Positive');
+        }
+    }
+}
+
+// signCheck(5, 12, -15);
+
+// signCheck(-6, -12, 14);
+
+signCheck(-1, -2, -3);
+
+// signCheck(-1, 0, 1);
+
+// signCheck(-5, 1, 1);
+
+
+
+/* function signChek(firstNum,secondNum,thirdNum){
+    let result = ''
+ 
+    if(firstNum * secondNum * thirdNum >= 0){
+        result='Positive'
+    }
+
+     if(firstNum * secondNum * thirdNum < 0){
+        result='Negative'
+    }
+    console.log(result);
+}
+ 
+signChek(5, 12, -15);
+ 
+signChek(-6, -12, 14);
+ 
+signChek(-1 ,-2,-3);
+ 
+signChek(-5 ,1 ,1); */
+
+
+
 /* function signCheck(numOne, numTwo, numThree) {
     let result = '';
 
@@ -35,30 +86,3 @@ signCheck(-1, -2, -3);
 // signCheck(-5, 1, 1);
 
  */
-
-function signCheck(numOne, numTwo, numThree) {
-    if (numOne > 0 && numTwo > 0 || numOne < 0 && numTwo < 0) {
-      if(numThree > 0){
-          console.log('Positive');
-      } else {
-        console.log('Negative');
-      }  
-    } else {
-        if(numThree > 0){
-            console.log('Negative');
-        } else {
-            console.log('Positive');
-        }  
-    }
-}
-
-// signCheck(5, 12, -15);
-
-// signCheck(-6, -12, 14);
-
-signCheck(-1, -2, -3);
-
-// signCheck(-1, 0, 1);
-
-// signCheck(-5, 1, 1);
-
--- JS_ADVANSED/Objects_and_Composition/rectangle.js
+++ JS_ADVANSED/Objects_and_Composition/rectangle.js
@@ -1,8 +1,17 @@
-function solve(width, height, color){
-    
+function rectangle(width, height, color) {
+    let firstChar = color[0].toUpperCase();
+    color = firstChar + color.substring(1);
+    let rect = {
+        width,
+        height,
+        color: color,
+        calcArea() {
+            let aria = this.width * this.height;
+            return aria;
+        }
+    };
+    return rect;
 }
-
-
 
 let rect = rectangle(4, 5, 'red');
 console.log(rect.width);
