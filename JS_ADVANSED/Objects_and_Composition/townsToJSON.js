727d8d87ce528bd7c66a750bc77565d88c5bef1a
twinsdz <twinsdz@yahoo.com>
2021-09-26 18:57:52 +0200

    update

--- JS_ADVANSED/Objects_and_Composition/townsToJSON.js
+++ JS_ADVANSED/Objects_and_Composition/townsToJSON.js
@@ -1,6 +1,42 @@
 function solve(array) {
     let arr = [];
 
+    let tableHead = array.shift().match(/[A-Za-z]+/g);
+    let [town, latit, longit] = tableHead;
+
+    for (let row of array) {
+        let obj = {}; // при всяко завъртане правя нов обект
+        let lineArr = row.match(/([^|]+)/g); // regex който реже всичко межди даден патерн
+        let [townName, latitNum, longitNum] = lineArr;
+
+        townName = townName.trim();
+        latitNum = Number(latitNum);
+        longitNum = Number(longitNum);
+        latitNum = Number(latitNum.toFixed(2));
+        longitNum = Number(longitNum.toFixed(2));
+
+        obj[town] = townName;
+        obj[latit] = latitNum;
+        obj[longit] = longitNum;
+        arr.push(obj);
+    }
+    return JSON.stringify(arr);
+}
+
+// console.log(solve(['| Town | Latitude | Longitude |',
+//     '| Sofia | 42.696552 | 23.32601 |',
+//     '| Beijing | 39.913818 | 116.363625 |'
+// ]));
+
+console.log(solve(['| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |'
+]));
+
+
+/* function solve(array) {
+    let arr = [];
+
     let tableHead = array.shift().split('|').filter(x => x !== '').map(x => x.trim());
     let [town, latit, longit] = tableHead;
 
@@ -26,7 +62,7 @@ console.log(solve(['| Town | Latitude | Longitude |',
     '| Sofia | 42.696552 | 23.32601 |',
     '| Beijing | 39.913818 | 116.363625 |'
 ]));
-
+ */
 
 /* function solve(array) {
     let arr = [];
