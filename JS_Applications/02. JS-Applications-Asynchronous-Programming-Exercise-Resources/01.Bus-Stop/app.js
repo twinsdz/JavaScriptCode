c5fe17b3a48bb9a21d36270c59f66eeb4bf30134
twinsdz <twinsdz@yahoo.com>
2021-11-04 23:58:15 +0100

    upload

--- JS_Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS_Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopNameEl = document.getElementById('stopName');
+    const timeTableEl = document.getElementById('buses');
+
+    const stopId = document.getElementById('stopId').value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+        stopNameEl.textContent = 'Loading...';
+        // timeTableEl.innerHTML = '';
+        timeTableEl.replaceChildren();
+        const res = await fetch(url);
+        if(res.status != 200){
+            throw new Error('Stop ID not found');
+        }
+        const data = await res.json();
+
+        stopNameEl.textContent = data.name;
+        Object.entries(data.buses).forEach(b => {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+            timeTableEl.appendChild(liElement);
+        });
+    } catch (error) {
+        stopNameEl.textContent = 'Error';
+    }
+
 }
