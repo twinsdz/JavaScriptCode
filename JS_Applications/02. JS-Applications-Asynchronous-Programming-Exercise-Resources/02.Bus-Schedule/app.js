e658f97b3edddae2dc86cce91b36a25949c09986
twinsdz <twinsdz@yahoo.com>
2021-11-06 00:20:50 +0100

    upload

--- JS_Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ JS_Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,28 @@
 function solve() {
+    const label = document.querySelector('#info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    let stop = {
+        next: 'depot'
+    };
 
-    function depart() {
-        console.log('Depart TODO...');
+   async function depart() {
+       departBtn.disabled = true; // изключвам бутона
+
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+
+        const res = await fetch(url); // заявка
+        stop = await res.json(); // присъиденява резултата от отговора на променливата стоп
+
+        label.textContent = `Next stop ${stop.name}`;
+        arriveBtn.disabled = false; // включвам другия бутон
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        label.textContent = `Arriving at ${stop.name}`;
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
